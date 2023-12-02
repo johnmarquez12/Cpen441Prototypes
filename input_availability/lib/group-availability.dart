e2bca31aba49454e09574ebf1002712a290122a4
chen-vv <vchen03@student.ubc.ca>
2023-12-01 17:10:50 -0800

    Finished group's best availability, no filter

--- input_availability/lib/group-availability.dart
+++ input_availability/lib/group-availability.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:input_availability/event.dart';
+import 'package:input_availability/tuple2.dart';
 import 'package:kalender/kalender.dart';
 import 'package:date_format/date_format.dart';
 
@@ -9,30 +11,142 @@ final CalendarController<Event> calendarController = CalendarController(
   initialDate: DateTime.now(),
 );
 
-final availabilities = [
-  DateTimeRange(
-    start: DateTime(2023, 12, 7, 15),
-    end: DateTime(2023, 12, 7, 16),
-  ),
-  DateTimeRange(
-    start: DateTime(2023, 12, 8, 11),
-    end: DateTime(2023, 12, 8, 12, 30),
-  ),
-  DateTimeRange(
-    start: DateTime(2023, 12, 9, 18, 30),
-    end: DateTime(2023, 12, 9, 20),
-  ),
-];
+const t = String.fromEnvironment("TEST");
+
+final availabilities = t == "easy"
+    ? [
+        DateTimeRange(
+          start: DateTime(2023, 12, 7, 15),
+          end: DateTime(2023, 12, 7, 16),
+        ),
+        DateTimeRange(
+          start: DateTime(2023, 12, 8, 11),
+          end: DateTime(2023, 12, 8, 12, 30),
+        ),
+        DateTimeRange(
+          start: DateTime(2023, 12, 9, 18, 30),
+          end: DateTime(2023, 12, 9, 20),
+        ),
+      ]
+    : t == "medium"
+        ? [
+            DateTimeRange(
+              start: DateTime(2023, 12, 7, 15),
+              end: DateTime(2023, 12, 7, 16),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 7, 17),
+              end: DateTime(2023, 12, 7, 18),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 8, 11),
+              end: DateTime(2023, 12, 8, 12, 30),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 8, 14),
+              end: DateTime(2023, 12, 8, 15, 30),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 9, 14, 30),
+              end: DateTime(2023, 12, 9, 15),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 9, 18, 30),
+              end: DateTime(2023, 12, 9, 20),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 10, 14, 30),
+              end: DateTime(2023, 12, 10, 16),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 10, 16, 30),
+              end: DateTime(2023, 12, 10, 17),
+            ),
+          ]
+        : [
+            DateTimeRange(
+              start: DateTime(2023, 12, 7, 13),
+              end: DateTime(2023, 12, 7, 14, 30),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 7, 15),
+              end: DateTime(2023, 12, 7, 16),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 7, 17),
+              end: DateTime(2023, 12, 7, 18),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 7, 19),
+              end: DateTime(2023, 12, 7, 20),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 8, 11),
+              end: DateTime(2023, 12, 8, 12, 30),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 8, 14),
+              end: DateTime(2023, 12, 8, 15, 30),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 8, 16),
+              end: DateTime(2023, 12, 8, 16, 30),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 8, 18),
+              end: DateTime(2023, 12, 8, 19, 30),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 9, 11, 30),
+              end: DateTime(2023, 12, 9, 13),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 9, 13, 30),
+              end: DateTime(2023, 12, 9, 14),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 9, 14, 30),
+              end: DateTime(2023, 12, 9, 15),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 9, 18, 30),
+              end: DateTime(2023, 12, 9, 20),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 10, 12, 30),
+              end: DateTime(2023, 12, 10, 14),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 10, 14, 30),
+              end: DateTime(2023, 12, 10, 16),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 10, 16, 30),
+              end: DateTime(2023, 12, 10, 17),
+            ),
+            DateTimeRange(
+              start: DateTime(2023, 12, 10, 18, 30),
+              end: DateTime(2023, 12, 10, 20),
+            ),
+          ];
 
 /// The number of available people, in the same order as the [DateTimeRange]s
 /// that appear in [availabilities].
-final participants = [
-  2,
-  3,
-  4,
-];
+final participants = t == "easy"
+    ? [
+        2,
+        3,
+        4,
+      ]
+    : t == "medium"
+        ? [6, 5, 7, 5, 7, 6, 5, 6]
+        : [12, 12, 13, 13, 14, 13, 15, 13, 14, 12, 13, 12, 14, 12, 15, 12];
 
-final totalParticipants = 4;
+const totalParticipants = t == "easy"
+    ? 4
+    : t == "medium"
+        ? 8
+        : 16;
 
 class GroupAvailability extends StatefulWidget {
   const GroupAvailability({super.key});
@@ -42,11 +156,21 @@ class GroupAvailability extends StatefulWidget {
 }
 
 class _GroupAvailabilityState extends State<GroupAvailability> {
+  var selectedTimeIndex = -1;
+  // Combine the lists using zip
+  final combinedList = List.generate(availabilities.length,
+      (index) => Tuple2(availabilities[index], participants[index]));
+
+  /// The max number of suggested times
+  final int maxSuggestedTimes = 3;
+
   @override
   void initState() {
     super.initState();
-    for (var a in availabilities) {
-      eventsController.addEvent(createCalendarEvent(a));
+    combinedList.sort((a, b) => b.participants.compareTo(a.participants));
+
+    for (var entry in combinedList) {
+      eventsController.addEvent(createCalendarEvent(entry.availability));
     }
   }
 
@@ -65,7 +189,7 @@ class _GroupAvailabilityState extends State<GroupAvailability> {
               child: SingleChildScrollView(
                 physics: const AlwaysScrollableScrollPhysics(),
                 scrollDirection: Axis.vertical,
-                reverse: true,
+                reverse: false,
                 child: ConstrainedBox(
                   constraints: BoxConstraints(
                     minHeight: MediaQuery.of(context).size.height / 10,
@@ -104,17 +228,50 @@ class _GroupAvailabilityState extends State<GroupAvailability> {
                 tileBuilder: _tileBuilder,
                 multiDayTileBuilder: _multiDayTileBuilder,
                 scheduleTileBuilder: _scheduleTileBuilder,
+                eventHandlers: CalendarEventHandlers(
+                  onEventTapped: onEventTapped,
+                ),
               ),
             ),
           ],
         ));
   }
 
+  CalendarEvent<Event> createCalendarEvent(DateTimeRange dateTimeRange) {
+    var index = 0;
+
+    for (var i = 0; i < combinedList.length; i++) {
+      if (combinedList[i].availability == dateTimeRange) {
+        index = i;
+        break;
+      }
+    }
+
+    var opacity = combinedList[index].participants / totalParticipants;
+    if (opacity < 1.0) {
+      opacity -= 0.2;
+    } else if (opacity <= 0.1) {
+      opacity = 0.1;
+    }
+
+    return CalendarEvent(
+      dateTimeRange: dateTimeRange,
+      modifiable: false,
+      eventData: Event(
+        // The custom object that you want to link to the event.
+        color: Colors.greenAccent.withOpacity(opacity),
+
+        timeRange:
+            "${combinedList[index].participants.toString()}/$totalParticipants",
+      ),
+    );
+  }
+
   List<Widget> getAvailabilityText() {
     final List<Widget> result = [];
 
-    for (var i = 0; i < availabilities.length; i++) {
-      final a = availabilities[i];
+    for (var i = 0; i < maxSuggestedTimes; i++) {
+      final a = combinedList[i].availability;
       var startHour = a.start.hour;
       var startMin = a.start.minute == 0 ? "00" : a.start.minute;
       var endHour = a.end.hour;
@@ -134,25 +291,45 @@ class _GroupAvailabilityState extends State<GroupAvailability> {
       final endFormatted = formatDate(
           a.end, [' to ', h, (endHour < 12 ? ":${endMin}am" : ":${endMin}pm")]);
       result.add(
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: RichText(
-            text: TextSpan(
-                text: "$startFormatted $endFormatted - ",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.black, // Add this line
-                ),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: "${participants[i].toString()}/$totalParticipants",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              eventsController.selectEvent(createCalendarEvent(a));
+              selectedTimeIndex = i;
+            });
+          },
+          child: Container(
+            decoration: i == selectedTimeIndex
+                ? BoxDecoration(
+                    color: Colors.greenAccent[200]?.withOpacity(0.5),
+                    border: Border.all(
+                      color: Colors.red,
+                      width: 2.5,
+                    ),
+                  )
+                : null,
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: RichText(
+                text: TextSpan(
+                    text: "$startFormatted $endFormatted - ",
+                    style: const TextStyle(
                       fontSize: 15,
                       color: Colors.black, // Add this line
                     ),
-                  )
-                ]),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text:
+                            "${combinedList[i].participants.toString()}/$totalParticipants",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.black, // Add this line
+                        ),
+                      )
+                    ]),
+              ),
+            ),
           ),
         ),
       );
@@ -160,19 +337,52 @@ class _GroupAvailabilityState extends State<GroupAvailability> {
 
     return result;
   }
+
+  Future<void> onEventTapped(CalendarEvent<Event> event) async {
+    HapticFeedback.lightImpact();
+
+    // Check if the event that was tapped is is currently selected.
+    setState(() {
+      eventsController.selectEvent(event);
+
+      var i = 0;
+
+      for (i = 0; i < combinedList.length; i++) {
+        final a = combinedList[i].availability;
+
+        if (event.dateTimeRange == a) {
+          break;
+        }
+      }
+
+      selectedTimeIndex = i;
+    });
+  }
 }
 
 Widget _tileBuilder(
     CalendarEvent<dynamic> event, TileConfiguration tileConfiguration) {
   final Event? customObject = event.eventData;
+  var fontSize = 11.0;
+
+  if (event.dateTimeRange.duration.compareTo(const Duration(hours: 1)) < 0) {
+    fontSize = 10.0;
+  }
 
   return Card(
     color: customObject?.color,
     shadowColor: Colors.transparent,
-    child: Center(
-      child: Text(
-        customObject!.timeRange,
-        style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+    child: Container(
+      decoration: BoxDecoration(
+        border: eventsController.selectedEvent == event
+            ? Border.all(color: Colors.red, width: 2)
+            : null,
+      ),
+      child: Center(
+        child: Text(
+          customObject!.timeRange,
+          style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold),
+        ),
       ),
     ),
   );
@@ -185,30 +395,3 @@ Widget _multiDayTileBuilder(event, tileConfiguration) => const Card(
 Widget _scheduleTileBuilder(event, date) => const Card(
       color: Colors.redAccent,
     );
-
-CalendarEvent<Event> createCalendarEvent(DateTimeRange dateTimeRange) {
-  var index = 0;
-
-  for (var i = 0; i < availabilities.length; i++) {
-    if (availabilities[i] == dateTimeRange) {
-      index = i;
-      break;
-    }
-  }
-
-  var opacity = participants[index] / totalParticipants;
-  if (opacity < 1.0) {
-    opacity -= 0.2;
-  }
-
-  return CalendarEvent(
-    dateTimeRange: dateTimeRange,
-    modifiable: false,
-    eventData: Event(
-      // The custom object that you want to link to the event.
-      color: Colors.greenAccent.withOpacity(opacity),
-
-      timeRange: "${participants[index].toString()}/$totalParticipants",
-    ),
-  );
-}
---
+++ input_availability/lib/tuple2.dart
@@ -0,0 +1,6 @@
+class Tuple2<A, B> {
+  final A availability;
+  final B participants;
+
+  Tuple2(this.availability, this.participants);
+}
